fe3c2a55fced364e38940791ed08b6952d1df480
Aviv Ben-Yosef <aviv@interactech.com>
2022-11-07 16:16:42 +0200

    initial -- final

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const res = require("express/lib/response");
+
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send("hello world")
+})
+
+app.get("/path/mypath", (req,res) => {
+    res.send("this is a path.")
+})
+
+app.get("/a/:num", (req,res) => {
+    res.send(req.params.num);
+})
+
+
+app.post("/b/:num", (req,res) => {
+    res.send(req.params.num);
+})
+
+app.listen(3000, () => {
+    console.log("server is running")
+})
